21f1f290ead396541eddafc0b5f09d52dd943d61
Franco Longo <129310461+Mintia11@users.noreply.github.com>
2026-08-11 19:00:24 +0200

    test

--- components/hisense_ac/hisense_ac.cpp
+++ components/hisense_ac/hisense_ac.cpp
@@ -51,86 +51,11 @@ void HisenseClimate::control(const climate::ClimateCall &call) {
 }
 
 void HisenseClimate::send_command_() {
-  // Generic DG11L1 14-byte payload state
-  uint8_t base_code[14] = {0xC3, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-
-  if (this->mode == climate::CLIMATE_MODE_OFF) {
-    uint8_t off_code[14] = {0xC3, 0x0C, 0xE0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xE3};
-    memcpy(base_code, off_code, 14);
-  } else {
-    uint8_t fan_val = 0x00;
-    switch (this->fan_mode.value_or(climate::CLIMATE_FAN_AUTO)) {
-      case climate::CLIMATE_FAN_LOW:    fan_val = 0x02; break;
-      case climate::CLIMATE_FAN_MEDIUM: fan_val = 0x01; break;
-      case climate::CLIMATE_FAN_HIGH:   fan_val = 0x00; break;
-      default:                          fan_val = 0x05; break; // Auto
-    }
-    
-    base_code[2] = fan_val | 0x80; // Overwrite fan mode
-
-    uint8_t temp = (uint8_t)this->target_temperature;
-    uint8_t mode_bits = 0;
-
-    // Map operation modes to binary and enforce forced temps
-    switch (this->mode) {
-      case climate::CLIMATE_MODE_HEAT:     mode_bits = 0x00; break;
-      case climate::CLIMATE_MODE_COOL:     mode_bits = 0x02; break;
-      case climate::CLIMATE_MODE_DRY:      mode_bits = 0x03; temp = 23; break; // Forced to 23
-      case climate::CLIMATE_MODE_FAN_ONLY: mode_bits = 0x04; temp = 23; break; // Forced to 23
-      default: mode_bits = 0x01; temp = 23; break;
-    }
-    
-    if (temp < 16) temp = 16;
-    if (temp > 31) temp = 31;
-
-    // Shift temp 4 bits left and append mode bits
-    base_code[3] = (((temp - 16) & 0x0F) << 4) | (mode_bits & 0x0F);
-
-    // XOR Checksum calculation for bytes 2 to 12
-    uint8_t checksum = 0;
-    for (int i = 2; i < 13; i++) {
-      checksum ^= base_code[i];
-    }
-    base_code[13] = checksum;
+  switch (this->mode) {
+    case: climate::CLIMATE_MODE_OFF:
+      ir_send_raw_pronto("0000 0073 0000 00ac 0138 009e 0015 003c 0015 003c 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003c 0015 0015 0015 003c 0015 003c 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003c 0015 003c 0015 003c 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003c 0015 0015 0015 003c 0015 003c 0015 003c 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0117 0015 003c 0015 0015 0015 0015 0015 003c 0015 0015 0015 0015 0015 0015 0015 003c 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003c 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003c 0015 0015 0015 003c 0015 003c 0015 0015 0015 003c 0015 003c 0015 0117 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003c 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003c 0015 0015 0015 003c 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003c 0015 0015 0015 0015 0015 003c 0015 0015 0015 003c 0015 0015 0015 0015 0015 0e4a");
+      break;
   }
-
-  std::vector<int32_t> timings;
-  timings.reserve(250);
-
-  // Derived Header Marks/Spaces (Converted from RAW_INIT broadlink ticks to microseconds)
-  timings.push_back(7695);  
-  timings.push_back(-3888); 
-
-  int bit_count = 0;
-  // Serialize bits into pulse bursts and spaces (LSB first per byte)
-  for (int b = 0; b < 14; b++) {
-    uint8_t byte_val = base_code[b];
-    for (int bit = 0; bit < 8; bit++) {
-      bool bit_val = (byte_val >> bit) & 0x01;
-
-      // Insert Segment Separators into the raw pulse train
-      // (104 and 234 in python correspond to bits 52 and 117 respectively)
-      if (bit_count == 52 || bit_count == 117) {
-        timings.push_back(540);   // EMPTY_TIME pulse (20 * 27us)
-        timings.push_back(-6885); // Segment Space (255 * 27us)
-      }
-
-      timings.push_back(540); // EMPTY_TIME pulse
-      if (bit_val) {
-        timings.push_back(-1485); // ONE_TIME space (55 * 27us)
-      } else {
-        timings.push_back(-540);  // ZERO_TIME space (20 * 27us)
-      }
-      bit_count++;
-    }
-  }
-
-  // Trailing pulse and space sequence
-  timings.push_back(540); 
-  timings.push_back(-89991); 
-
-  // Fire the hardware transmission command
-  ir_send_raw(timings.data(), timings.size());
 }
 
 }  // namespace hisense_ac
