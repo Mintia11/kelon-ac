3a8a07a95ac2547c7623a9f7626c025957cd1e83
Franco Longo <129310461+Mintia11@users.noreply.github.com>
2026-08-11 18:36:58 +0200

    Use the right ir codes

--- components/kelon_ac/climate.py
+++ components/kelon_ac/climate.py
@@ -3,31 +3,15 @@ import esphome.config_validation as cv
 from esphome.components import climate
 from esphome.const import CONF_ID
 
-CODEOWNERS = ["@you"]
+# Define the C++ namespace and class
+hisense_ac_ns = cg.esphome_ns.namespace("hisense_ac")
+HisenseClimate = hisense_ac_ns.class_("HisenseClimate", climate.Climate, cg.Component)
 
-kelon_ac_ns = cg.esphome_ns.namespace("kelon_ac")
-KelonAcClimate = kelon_ac_ns.class_("KelonAcClimate", climate.Climate, cg.Component)
-KelonProtocolVariant = kelon_ac_ns.enum("KelonProtocolVariant")
+# Extend the standard climate schema
+CONFIG_SCHEMA = climate.climate_schema(HisenseClimate).extend(cv.COMPONENT_SCHEMA)
 
-CONF_PROTOCOL = "protocol"
-
-PROTOCOLS = {
-    "KELON": KelonProtocolVariant.PROTOCOL_KELON,
-    "KELON168": KelonProtocolVariant.PROTOCOL_KELON168,
-}
-
-CONFIG_SCHEMA = climate.climate_schema(KelonAcClimate).extend(cv.COMPONENT_SCHEMA).extend(
-    {
-        # KELON    = basic 48-bit "ON/OFF 9000-12000" series
-        # KELON168 = 168-bit Hisense / DG11R2-01 remote series
-        cv.Optional(CONF_PROTOCOL, default="KELON168"): cv.enum(
-            PROTOCOLS, upper=True
-        ),
-    }
-)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
-    cg.add(var.set_protocol(config[CONF_PROTOCOL]))
